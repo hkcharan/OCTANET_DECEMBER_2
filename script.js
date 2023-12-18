2d73eca83f584836ea1442379ca741b586a54bd0
H K Charan <120408537+hkcharan@users.noreply.github.com>
2023-12-18 14:56:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+const input = document.getElementById("input");
+const listContainer = document.getElementById("list-container")
+
+function addTask(){
+    if(input.value === ''){
+        alert("You must write something");
+    }
+
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = input.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.style.backgroundImage = "url(images/cross.png)";
+        li.appendChild(span);
+    }
+    input.value = " ";
+    saveData();
+}
+
+listContainer.addEventListener('click', function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
